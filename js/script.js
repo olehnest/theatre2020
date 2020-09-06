9264cc23110bd310cde2790f6ecae98fb1432802
olehnest <69309206+olehnest@users.noreply.github.com>
2020-09-06 20:00:56 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,24 +2,24 @@ const text = document.querySelector(".fancy");
 const strText = text.textContent;
 const splitText = strText.split("");
 text.textContent = "";
-for(let i=0; i < splitText.length; i++) {
-    text.innerHTML += "<span>" + splitText[i] + "</span>";
+for (let i = 0; i < splitText.length; i++) {
+  text.innerHTML += "<span>" + splitText[i] + "</span>";
 }
 
 let char = 0;
 let time = setInterval(onTick, 50);
 
-function onTick(){
-    const span = text.querySelectorAll('span')[char];
-    span.classList.add('fade');
-    char++
-    if(char === splitText.length){
-        OfflineAudioCompletionEvent();
-        return;
-    }
+function onTick() {
+  const span = text.querySelectorAll("span")[char];
+  span.classList.add("fade");
+  char++;
+  if (char === splitText.length) {
+    OfflineAudioCompletionEvent();
+    return;
+  }
 }
 
-function complete(){
-    clearInterval(timer);
-    timer = null;
+function complete() {
+  clearInterval(timer);
+  timer = null;
 }

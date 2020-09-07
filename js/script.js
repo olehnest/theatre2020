28479a52aeef1c6e4f8aa6446c13ec8314ad0915
olehnest <oleg.nesterenko9@gmail.com>
2020-09-07 18:15:07 +0000

    Final Version

--- js/script.js
+++ js/script.js
@@ -1,10 +1,22 @@
+var $j = jQuery.noConflict();
+
 const loader = document.querySelector(".loader");
     window.onload = function(){
       setTimeout(function(){
-        loader.style.opacity = "0";
+        loader.style.opacity = 0;
         setTimeout(function(){
           loader.style.display = "none";
         }, 300);
       },1000);
-    }
+    };
 
+$('.cardtest').hover(function(){
+if($(this).hasClass("active")){
+  $('.cardtest .bottomtest').slideUp(function(){
+    $('.cardtest').removeClass("active");
+  });
+}else{
+  $('.cardtest').addClass("active");
+  $('.cardtest .bottomtest').stop().slideDown();
+}
+});
